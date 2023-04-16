a776036fa7074dd4942da57cd50f905867756602
YiLan21 <gsn4466@gmail.com>
2023-04-17 00:03:33 +0800

    test

---
+++ src/main/java/com/example/demo_0416_ks/Demo0416KsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_0416_ks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0416KsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo0416KsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_0416_ks/Demo0416KsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_0416_ks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo0416KsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
